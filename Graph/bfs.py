a6a8f48d672964db7d6b1303a1fe33c1623dc5a4
Akash Kumar <120348515+AkashKumar2511@users.noreply.github.com>
2024-05-20 14:20:01 +0530

    Update bfs.py

--- Graph/bfs.py
+++ Graph/bfs.py
@@ -1,3 +1,21 @@
+"""
+Algorithm:
+---------
+1. Use a boolean list to mark all the vertices as not visited.
+2. Initially mark first vertex as visited(true).
+3. Create a queue for BFS and push first vertex in queue.
+4. While queue is not empty:
+   I. Keep adding front element in output list and popping it from queue.
+   II. Traverse over all the connected components of front element.
+   III. If they aren't visited, mark them visited and add to queue.
+5. Return the output list.
+
+
+Time Complexity: O(V + E)
+Space Complexity: O(V)
+"""
+
+
 #starting node : 0
 def bfs(adj, start):
   vis = [start] * v
